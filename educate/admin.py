963d4d2c8ac9ccdabbb6bf1c94761362d9445a8e
Tim Barnes <tw@timandmarina.com>
2014-12-02 09:09:28 -0800

    Admin enhancements - filter for categories (not working yet)

--- educate/admin.py
+++ educate/admin.py
@@ -3,6 +3,9 @@ from educate.models import Subject, Category, Question, Article
 
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_filter = ('subject__name',)
+
 class QuestionAdmin(admin.ModelAdmin):
     list_filter = ('category')
     list_display = ('question_text', 'answer_text')
