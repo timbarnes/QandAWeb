e34d812ab4ae80233257cd4c2a3af4dcb4d8af38
Tim Barnes <tbarnes@bulldog.dreamhost.com>
2014-12-04 21:13:14 -0800

    Minor fixes to prepare for sync with dev - taggit, pyMySQL, collectstatic

--- dj/settings.py
+++ dj/settings.py
@@ -101,4 +101,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, parms['static-root'])
 STATIC_URL = '/static/'
